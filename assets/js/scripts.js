6b1bdff0185234feb6863c1d59b5d194dac30d2d
Santiago Brito <santiagobrito0615@gmail.com>
2022-09-15 01:00:19 -0300

    cambios

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -92,3 +92,20 @@ document.addEventListener('DOMContentLoaded', () => {
    }, 9000)
 
 })
+function count(){
+   var counter = { var: 0 };
+   TweenMax.to(counter, 3, {
+     var: 200, 
+     onUpdate: function () {
+       var number = Math.ceil(counter.var);
+       $('.counter').html(number);
+       if(number === counter.var){ count.kill(); }
+     },
+     onComplete: function(){
+       count();
+     },    
+     ease:Circ.easeOut
+   });
+ }
+ 
+ count();
